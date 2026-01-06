970d529c0508ffe364f10fbf225688b025f772a3
YANG LIU <1513457732@qq.com>
2026-01-06 14:58:46 +0800

    Delete EASE-（一凑二） （wt20类93.84) directory

--- EASE-（一凑二） （wt20类93.84)/utils/factory.py
+++
@@ -1,8 +0,0 @@
-def get_model(model_name, args):
-    name = model_name.lower()
-    if name == "ease":
-        from models.ease import Learner
-    else:
-        assert 0
-    
-    return Learner(args)
--- EASE-（一凑二） （wt20类93.84)/测试.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# 假设文件路径为 'data/BeiJiao/train/0.npy'
-file_path = 'data/BeiJiao/test/1.npy'
-data = np.load(file_path)
-print(f"Data shape: {data.shape}")
-print(f"First few rows of data:\n{data[:5]}")
